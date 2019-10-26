e1f7e6d390091d58b61e1c2bc74c0c393888a160
B4rtlomiej <b.badora@outlook.com>
2019-10-26 21:12:22 +0200

    Rename Ticket table to Tickets

---
+++ Migrations/20191026191012_RenameTicketTableToTickets.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace servicer.API.Migrations
+{
+    public partial class RenameTicketTableToTickets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Items_ItemId",
+                table: "Ticket");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTicket_Ticket_TicketId",
+                table: "UserTicket");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ticket",
+                table: "Ticket");
+
+            migrationBuilder.RenameTable(
+                name: "Ticket",
+                newName: "Tickets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ticket_ItemId",
+                table: "Tickets",
+                newName: "IX_Tickets_ItemId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tickets",
+                table: "Tickets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Items_ItemId",
+                table: "Tickets",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTicket_Tickets_TicketId",
+                table: "UserTicket",
+                column: "TicketId",
+                principalTable: "Tickets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Items_ItemId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTicket_Tickets_TicketId",
+                table: "UserTicket");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tickets",
+                table: "Tickets");
+
+            migrationBuilder.RenameTable(
+                name: "Tickets",
+                newName: "Ticket");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tickets_ItemId",
+                table: "Ticket",
+                newName: "IX_Ticket_ItemId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ticket",
+                table: "Ticket",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Items_ItemId",
+                table: "Ticket",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTicket_Ticket_TicketId",
+                table: "UserTicket",
+                column: "TicketId",
+                principalTable: "Ticket",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
